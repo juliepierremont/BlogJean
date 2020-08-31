26c503d56989380f17e751b474774dd490ca348e
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-08-31 15:06:36 +0200

    Affichage connection
    Mise en place de la connection à la bdd et affichage sur la home

---
+++ modele/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database
+
+{
+    const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
+    const DB_USER = 'root';
+    const DB_PASS = 'root';
+
+
+    public function getConnection ()
+    
+    {
+        try{
+            $connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return 'Connexion OK';
+        }
+        
+        catch(Exception $errorConnection)
+        {
+            die ('Erreur de connection :'.$errorConnection->getMessage());
+            
+        }
+    }
+    
+    
+}
