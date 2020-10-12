8d08ca6b40c66511ba5e0a3462b8a57772a66104
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-10-12 11:42:18 +0200

    Create viewChapter.php
    Création de la vue chapitre

---
+++ view/viewChapter.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Chapitre <?= htmlspecialchars($articles['numberChapter']); ?> <?= htmlspecialchars($articles['title']); ?></title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
