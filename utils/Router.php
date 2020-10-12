c491ca05a55d5e8225ba7463d77e18ca657947c9
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-10-12 11:41:25 +0200

    Ajout chapter et comment
    CRéation des fonctions chapter et comment

--- utils/Router.php
+++ utils/Router.php
@@ -2,15 +2,47 @@
 
 namespace App\utils;
 
+use App\src\controller\FrontController;
+
 class Router
 {
+    private $frontcontoller;
+    public function __construct()
+    {
+        $this->frontcontroller = new FrontController();
+    }
+
     public function run()
     {
         if (isset($_GET['url'])) {
+
             var_dump($_GET['url']);
-            if (isset($_GET['id'])) {
-                var_dump($_GET['id']);
+            //$get=$_GET['get'];
+
+            if ($_GET['chapter'] == 'chapter') {
+                $this->frontcontoller->getChapter($_GET['chapter']);
             }
+
+            if ($_GET['addComment'] == 'addComment') {
+                $this->frontcontoller->addComment();
+            }
+
+            if ($_GET['connection'] == 'connection') {
+            }
+
+            if ($_GET['register'] == 'register') {
+            }
+
+            if ($_GET['administration'] == 'administration') {
+            }
+
+            if ($_GET['disconnection'] == 'disconnection') {
+            }
+
+            if ($_GET['delectCount'] == 'deleteCount') {
+            }
+        } else {
+            $this->frontcontroller->home();
         }
     }
 }
