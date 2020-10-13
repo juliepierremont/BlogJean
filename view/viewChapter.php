247e49fe1b24db460fea4172c8368c6d78890b21
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-10-13 09:13:46 +0200

    Update viewChapter.php
    Mise à jour de la vue chapter

--- view/viewChapter.php
+++ view/viewChapter.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="utf-8">
-    <title>Chapitre <?= htmlspecialchars($articles['numberChapter']); ?> <?= htmlspecialchars($articles['title']); ?></title>
-</head>
-
-<body>
-<section class=chapter>
-            <div>
-                <h2><?= htmlspecialchars($articles['title']); ?></h2>
+<?php var_dump($chapter) ?>
 
 
-            </div>
-</section>  
+<h2><?= htmlspecialchars($chapter['title']); ?></h2>
 
-<section class=comment>
-            <div>
-                <h2><?= htmlspecialchars($articles['title']); ?></h2>
-
-
-            </div>
-</section>  
-
-
-</body>
-
-</html>
