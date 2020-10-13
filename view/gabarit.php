b4ec3785d085f5b71d9bcde3971c62444294ec23
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-10-13 09:13:31 +0200

    Create gabarit.php
    Création d'un gabarit pour le header et footer

---
+++ view/gabarit.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Bienvenue sur le blog de Jean Forteroche</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Billet simple pour l'Alaska</h1>
+        <p>Découvrez l'Alaska sous une autre aurore.</p>
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">"Accueil"</a>
+                    <i class="fas fa-igloo"></i>
+                </li>
+                <li>
+                    <a href="index.php">"Chapitres"</a>
+                    <i class="fas fa-book"></i>
+                </li>
+                <li>
+                    <a href="index.php">"Connexion"</a>
+                    <i class="fas fa-sign-in-alt"></i>
+                </li>
+
+            </ul>
+        </nav>
+
+    </header>
+
+<?= $content; ?>
+
+</body>
+
+</html>
