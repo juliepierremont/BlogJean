5e031bce82807ad89cf6c3a52ac80a82e9aa7aef
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-10-26 20:03:22 +0100

    Update viewChapter.php
    Correction de la methode post du formulaire

--- view/viewChapter.php
+++ view/viewChapter.php
@@ -1,14 +1,17 @@
 <h2><?= htmlspecialchars($chapter['title']); ?></h2>
+<p><?= htmlspecialchars($chapter['content']); ?></p>
+
+<h2>Poster un commentaire</h2>
+
+<form action="index.php?url=addComment&id=<?= $chapter['id'] ?>" method="post">
+
+    <input type="text" name="author" id="author" placeholder="Nom">
+    <input type="text" name="comment" id="comment" placeholder="Commentaire">
+    <input type="submit" value="envoyer">
+
+</form>
 
 <?php foreach ($comments as $comment) { ?>
     <h2><?= htmlspecialchars($comment['author']); ?></h2>
     <p><?= htmlspecialchars($comment['content']); ?></p>
 <?php } ?>
-
-<form action="http://localhost/BlogJean/index.php?url=addComment&id=1" method="post"> 
-
-<input type="text" name="author" id="author">
-<input type="text" name="content" id="content">
-<input type="submit" value="envoyer">
-
-</form>
