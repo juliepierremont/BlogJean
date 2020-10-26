56771c60c783fe337ee4c26136d528240e014be9
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-10-26 10:31:31 +0100

    Update viewChapter.php
    correction du chapter

--- view/viewChapter.php
+++ view/viewChapter.php
@@ -1,5 +1,14 @@
-<?php var_dump($chapter) ?>
-
-
 <h2><?= htmlspecialchars($chapter['title']); ?></h2>
 
+<?php foreach ($comments as $comment) { ?>
+    <h2><?= htmlspecialchars($comment['author']); ?></h2>
+    <p><?= htmlspecialchars($comment['content']); ?></p>
+<?php } ?>
+
+<form action="http://localhost/BlogJean/index.php?url=addComment&id=1" method="post"> 
+
+<input type="text" name="author" id="author">
+<input type="text" name="content" id="content">
+<input type="submit" value="envoyer">
+
+</form>
