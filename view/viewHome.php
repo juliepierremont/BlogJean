a405b47d3216b4d50b2b3e9ad756d181660da30a
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-10-26 10:31:50 +0100

    Update viewHome.php
    optimisation de la view home

--- view/viewHome.php
+++ view/viewHome.php
@@ -1,28 +1,16 @@
+<div>
+    <h2>Les derniers chapitres</h2>
 
     <div>
-        <h2>Les derniers chapitres</h2>
-
-        <!--  <?php
-
-                var_dump($articles);
-                ?>  -->
-        <!-- $article = new Article();
-    $articles = $article->getArticles();
-    while($article = $articles->fetch()) -->
-
-        <div>
-            <h2><?= htmlspecialchars($articles['title']); ?></h2>
-            <p><?= htmlspecialchars($articles['content']); ?></p>
-            <p><?= htmlspecialchars($articles['author']); ?></p>
-            <p><?= htmlspecialchars($articles['numberChapter']); ?></p>
-            <p>Créé le : <?= htmlspecialchars($articles['createdAt']); ?></p>
-        </div>
-        <br>
-        <!-- <form action="index.php?action=allChapters&id=<?= $data['id'] ?>" method="post"> -->
-            <button type="submit">Lire la suite</button>
-
-            <!-- <?php
-
-                    $articles->closeCursor()
-                    ?>  -->
+        <h2><?= htmlspecialchars($articles['title']); ?></h2>
+        <p><?= htmlspecialchars($articles['content']); ?></p>
+        <p><?= htmlspecialchars($articles['author']); ?></p>
+        <p><?= htmlspecialchars($articles['numberChapter']); ?></p>
+        <p>Créé le : <?= htmlspecialchars($articles['createdAt']); ?></p>
     </div>
+    <br>
+
+    <button onclick="window.location.href = 'http://localhost/BlogJean/index.php?url=chapter&id=1'; "> Lire la suite</button>
+
+
+</div>
