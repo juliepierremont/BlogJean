b523781b76427bf62822e0ebaa94a8a57a0d5ed2
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-11-17 17:18:58 +0100

    Update Chapter.php
    optimisation du chpater en supprimant la partie commentaire

--- modele/Chapter.php
+++ modele/Chapter.php
@@ -30,28 +30,28 @@ class Chapter
         return $result;
     }
 
-    public function getComment($chapterID)
-    {
-        $connection = $this->db->getConnection();
-        $req = $connection->prepare('SELECT id, author, content FROM comment WHERE article_id = ? ORDER BY commentDate DESC');
-        $req->execute(array($chapterID));
-        while($data=$req->fetch()){
-            $comments[]=$data;
-        }
-        return $comments;
-    }
+    // public function getComment($chapterID)
+    // {
+    //     $connection = $this->db->getConnection();
+    //     $req = $connection->prepare('SELECT id, author, content FROM comment WHERE article_id = ? ORDER BY commentDate DESC');
+    //     $req->execute(array($chapterID));
+    //     while($data=$req->fetch()){
+    //         $comments[]=$data;
+    //     }
+    //     return $comments;
+    // }
 
-    public function addComment($id, $author, $content)
-    {
-        $connection = $this->db->getConnection();
-        $req = $connection->prepare('INSERT INTO comment(article_id, author, content, commentDate) VALUES (:article_id, :author, :content, NOW())');
-        $req->execute(array(
-            'article_id' => $id,
-            'author' => $author,
-            'content' => $content
-        ));
-        header('location:index.php?url=chapter&id=' . $id );
-    }
+    // public function addComment($id, $author, $content)
+    // {
+    //     $connection = $this->db->getConnection();
+    //     $req = $connection->prepare('INSERT INTO comment(article_id, author, content, commentDate) VALUES (:article_id, :author, :content, NOW())');
+    //     $req->execute(array(
+    //         'article_id' => $id,
+    //         'author' => $author,
+    //         'content' => $content
+    //     ));
+    //     header('location:index.php?url=chapter&id=' . $id );
+    // }
 
 
 
