ce135d14f204defc2ce3e06d3469e8b6757b8000
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-11-17 16:53:21 +0100

    Update viewHome.php
    optimisation de la view home grace à ajout du chaptermodel

--- view/viewHome.php
+++ view/viewHome.php
@@ -1,19 +1,15 @@
 <div>
     <h2>Les derniers chapitres</h2>
 
-    <div>
-        <h2><?= htmlspecialchars($articles['title']); ?></h2>
-        <p><?= htmlspecialchars($articles['content']); ?></p>
-        <p><?= htmlspecialchars($articles['author']); ?></p>
-        <p><?= htmlspecialchars($articles['numberChapter']); ?></p>
-        <p>Créé le : <?= htmlspecialchars($articles['createdAt']); ?></p>
-    </div>
-    <br>
-    <h2>Tous les chapitres</h2>
-    <p>chapitre 1</p>
-    <a href= 'index.php?url=chapter&id=1'; > Lire la suite</a>
-    <p>chapitre 2</p>
-    <a href= 'index.php?url=chapter&id=2'; > Lire la suite</a>
-    <p>chapitre 3</p>
-    <a href= 'index.php?url=chapter&id=3'; > Lire la suite</a>
+    <?php foreach ($chapters as $chapter) { ?>
+
+        <h2><a href="index.php?url=chapter&id=<?= $chapter->getId() ?>">
+                <?= htmlspecialchars($chapter->getTitle()); ?> </a></h2>
+        <p><?= htmlspecialchars($chapter->getContent()); ?></p>
+        <p><?= htmlspecialchars($chapter->getAuthor()); ?></p>
+        <p><?= htmlspecialchars($chapter->getNumberChapter()); ?></p>
+        <p>Créé le : <?= htmlspecialchars($chapter->getCreatedAt()); ?></p>
+    <?php } ?>
+
+
 </div>
