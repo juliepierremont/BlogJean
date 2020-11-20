bac64a84f38710254d75dc35a88ad306ac10d2e7
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-11-20 10:33:34 +0100

    Update viewProfile.php
    ajout des liens vers les éléments du profil

--- view/viewProfile.php
+++ view/viewProfile.php
@@ -1,10 +1,27 @@
-
 <h1>Mon blog</h1>
 <p>En construction</p>
 <div>
-    <h2><?= $this->session->get('pseudo'); ?></h2>
-    <p><?= $this->session->get('id'); ?></p>
-    <a href="../public/index.php?route=updatePassword">Modifier son mot de passe</a>
+    <h2>Profil de <?= $_SESSION['username'];
+                    var_dump($_SESSION); ?></h2>
+    <p><?= $_SESSION['id'] ?></p>
+    <!-- <a href="../public/index.php?route=updatePassword">Modifier son mot de passe</a> -->
 </div>
+
+<h3>Commentaires</h3>
+<p>Editer les commentaires</p>
+<p>Supprimer des commentaires</p>
+
+
+<h3>Chapitre</h3>
+<p>Créer une chapitre</p>
+<p>Editer un chapitre</p>
+<p>Supprimer un chapiter</p>
+
+<h3>Mon profil</h3>
+<p>Editer mon profil</p>
+
 <br>
-<a href="../public/index.php">Retour à l'accueil</a>
+<a href="index.php">Retour à l'accueil</a>
+
+
+
