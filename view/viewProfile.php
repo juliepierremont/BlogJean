64ade2f7736cdd41adb8f8c384c8429ccc7259a1
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-11-24 09:13:35 +0100

    Update viewProfile.php
    mise à jour de la vue profil au niveau des url

--- view/viewProfile.php
+++ view/viewProfile.php
@@ -1,27 +1,23 @@
 <h1>Mon blog</h1>
 <p>En construction</p>
 <div>
-    <h2>Profil de <?= $_SESSION['username'];
-                    var_dump($_SESSION); ?></h2>
-    <p><?= $_SESSION['id'] ?></p>
-    <!-- <a href="../public/index.php?route=updatePassword">Modifier son mot de passe</a> -->
+
+    <?php print_r($_SESSION);?>
+
 </div>
 
 <h3>Commentaires</h3>
-<p>Editer les commentaires</p>
-<p>Supprimer des commentaires</p>
+<p><a href="index.php/editComment">Editer les commentaires</a></p>
+<p><a href="index.php/deleteComment">Supprimer des commentaires</a></p>
 
 
 <h3>Chapitre</h3>
 <p>Créer une chapitre</p>
 <p>Editer un chapitre</p>
-<p>Supprimer un chapiter</p>
+<p>Supprimer un chapitre</p>
 
 <h3>Mon profil</h3>
-<p>Editer mon profil</p>
+<p> <a href="index.php?url=admin&action=editProfile">Editer mon profil</a></p>
 
 <br>
 <a href="index.php">Retour à l'accueil</a>
-
-
-
