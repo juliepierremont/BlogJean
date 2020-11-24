c805ed79344881f5cb4726a9a9bd465eed5c41f1
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-11-24 09:10:06 +0100

    Create EditProfile.php
    création du modele pour editer le profil

---
+++ modele/EditProfile.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\modele;
+
+use App\utils\Database;
+
+class EditProfile
+{
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getProfile($id)
+    {
+        $connection = $this->db->getConnection();
+        $req = $connection->prepare('SELECT id, pseudo, email, DATE_FORMAT(date_inscription, \'%d/%m/%Y\') AS creation_date_fr  FROM members WHERE id = ?');
+        $req->execute(array($id));
+        $data = $req->fetch();
+        return new Connection($data);
+        $req->closeCursor();
+    }
+
+    public function updatePseudo($newPseudo, $id)
+    {
+        $connection = $this->db->getConnection();
+        $req = $connection->prepare('UPDATE members SET pseudo = ? WHERE id = ?');
+        $req->execute(array($newPseudo, $id));
+        $req->closeCursor();
+    }
+
+    public function updateEmail($newEmail, $id)
+    {
+        $connection = $this->db->getConnection();
+        $req = $connection->prepare('UPDATE members SET email = ? WHERE id = ?');
+        $req->execute(array($newEmail, $id));
+        $req->closeCursor();
+    }
+
+
+    public function updatePassword($newPassword, $id)
+    {
+        $connection = $this->db->getConnection();
+        $req = $connection->prepare('UPDATE members SET pass = ? WHERE id = ?');
+        $req->execute(array($newPassword, $id));
+        $req->closeCursor();
+    }
+}
