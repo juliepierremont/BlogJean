1dcf2a4bba0273373100259d463ad24e5082d559
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-12-08 09:16:07 +0100

    correction des champs
    correction des champs pseudo pour adéquation avec la bdd

--- view/viewEditProfile.php
+++ view/viewEditProfile.php
@@ -1,12 +1,12 @@
 <!-- Edition du profil -->
 <div class="container-editprofile bg-light">
-    <div class="container col-lg-3 style-container"> 
+    <div class="container col-lg-3 style-container">
         <h2 class="style-contact-title text-center">Edition du profil :</h2>
         <form action="editprofile" method="POST" class="form-register">
             <div class="col-lg-12">
                 <div class="form-group">
                     <label for="inputnewpseudo">Nouveau pseudo :</label>
-                    <input type="text" name="newPseudo" class="form-control" id="inputnewpseudo" value="<?= $_SESSION['pseudo'] ?>">
+                    <input type="text" name="newPseudo" class="form-control" id="inputnewpseudo" value="<?= $_SESSION['username'] ?>">
                 </div>
             </div>
             <div class="col-lg-12">
@@ -29,6 +29,8 @@
                 <button type="submit" name="submit" class="btn btn-primary btn-block">Mettre à jour le profil</button>
             </div>
         </form>
-        <p class="text-center text-danger font-weight-bold style-register-error"><?php if(isset($error)) { echo $error; }?></p>
-   </div> 
+        <p class="text-center text-danger font-weight-bold style-register-error"><?php if (isset($error)) {
+                                                                                        echo $error;
+                                                                                    } ?></p>
+    </div>
 </div>
