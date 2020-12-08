696a0c565624f31156880e88e7ab37723d206dea
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-12-08 12:17:31 +0100

    Update EditComment.php
    tentative d'amélioration du modèle d'édition des commentaires

--- modele/EditComment.php
+++ modele/EditComment.php
@@ -4,11 +4,29 @@ namespace App\modele;
 
 use App\utils\Database;
 
-class EditProfile {
+class EditComment
+{
     public function __construct()
     {
         $this->db = new Database();
     }
 
+    public function getComment($chapterID)
+    {
+        $connection = $this->db->getConnection();
+        $req = $connection->prepare('SELECT id, author, content FROM comment WHERE article_id = ? ORDER BY commentDate DESC');
+        $req->execute(array($chapterID));
+        while($data=$req->fetch()){
+            $comments[]=$data;
+        }
+        return $comments;
+    }
 
+    public function updateComment($newComment, $id)
+    {
+        $connection = $this->db->getConnection();
+        $req = $connection->prepare('UPDATE comment SET content = ? WHERE id = ?');
+        $req->execute(array($newComment, $id));
+        $req->closeCursor();
+    }
 }
