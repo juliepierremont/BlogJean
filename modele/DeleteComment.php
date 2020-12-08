0078573fbe0b943bb81498411687b4b60eb6e9b0
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-12-08 12:16:29 +0100

    Update DeleteComment.php
    Tentative de debug du modèle de suppression des commentaires

--- modele/DeleteComment.php
+++ modele/DeleteComment.php
@@ -4,11 +4,33 @@ namespace App\modele;
 
 use App\utils\Database;
 
-class EditProfile {
+class DeleteComment
+{
     public function __construct()
     {
         $this->db = new Database();
     }
 
+    public function getComment($chapterID)
+    {
+        $connection = $this->db->getConnection();
+        $req = $connection->prepare('SELECT id, author, content FROM comment WHERE article_id = ? ORDER BY commentDate DESC');
+        $req->execute(array($chapterID));
+        while ($data = $req->fetch()) {
+            $comments[] = $data;
+        }
+        return $comments;
+    }
 
+    public function deleteComment($commentID)
+    {
+        $connection = $this->db->getConnection();
+        $req = $connection->prepare('DELETE FROM comment(comment_id, author, content, commentDate, article_id) WHERE id = ' . $commentID);
+        $req->execute(array(
+            'comment_id' => $id,
+            'author' => $author,
+            'content' => $content
+        ));
+        header('location:index.php?url=chapter&id=' . $id);
+    }
 }
