54c35923bdb72f54bec03721ef972b9f48a723fc
juliepierremont <53823333+juliepierremont@users.noreply.github.com>
2020-12-14 16:10:33 +0100

    Ajout des fonctions admin
    Déplacement des fonctions administration

--- src/controller/BackController.php
+++ src/controller/BackController.php
@@ -4,6 +4,12 @@ namespace App\src\controller;
 
 use App\modele\Connection;
 use App\utils\View;
+use App\modele\EditProfile;
+use App\modele\EditComment;
+use App\modele\DeleteComment;
+use App\modele\CreateChapter;
+use App\modele\DeleteChapter;
+use App\modele\UpdateChapter;
 
 class BackController
 {
@@ -16,11 +22,10 @@ class BackController
     {
         if (!empty($post['login']) && !empty($post['password'])) {
             $connection = $this->connection->getConnection($post['login'], $post['password']);
-            $this->checkPassword($connection,$post);
+            $this->checkPassword($connection, $post);
         }
     }
 
-    
     public function checkPassword($checkPassword, $checkUser)
     {
         if ($checkPassword) {
@@ -34,5 +39,51 @@ class BackController
             echo 'Mauvais identifiant ou mot de passe !';
         }
     }
+    
+    public function getProfile()
+    {
+        $myView = new View('viewProfile');
+        $myView->render();
+    }
 
+    public function editComment()
+    {
+        $myView = new View('viewEditComment');
+        $myView->render();
+    }
+
+    public function deleteComment()
+    {
+        $myView = new View('viewDeleteComment');
+        $myView->render();
+    }
+
+    public function createChapter()
+    {
+        $myView = new View('viewCreateChapter');
+        $myView->render();
+    }
+    public function deleteChapter()
+    {
+        $myView = new View('viewDeleteChapter');
+        $myView->render();
+    }
+
+    public function updateChapter()
+    {
+        $myView = new View('viewUpdateChapter');
+        $myView->render();
+    }
+
+    public function editProfile()
+    {
+        $myView = new View('viewEditProfile');
+        $myView->render();
+    }
+
+    public function editChapter()
+    {
+        $myView = new View('viewEditChapter');
+        $myView->render();
+    }
 }
